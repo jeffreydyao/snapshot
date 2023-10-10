381a53c1e2a24baa880e0a31de1be747d742b7a8
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2023-10-11 01:17:02 +1100

    docs: document Behavior model

--- Snapshot/Models/Behavior.swift
+++ Snapshot/Models/Behavior.swift
@@ -8,10 +8,27 @@
 import Foundation
 
 /**
- Represents a behavior with a name and a timestamp.
+ A behavior logged by a user.
  */
 struct Behavior: Identifiable, Hashable {
+    /**
+     A unique identifier for the instance of the logged behavior.
+     */
     let id = UUID()
+    /**
+     The name of the behavior.
+     ## Example
+     Eat food
+     */
     var name: String
+    /**
+     An optional note providing extra details about the behavior.
+     ## Example
+     200g steak w/ a glass of water
+     */
+    var note: String?
+    /**
+     The time at which the behavior was logged.
+     */
     var timestamp: Date
 }
